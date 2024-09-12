b220723a17f7b2a572a9b897e7239c9c4c66705f
lolosaisa <lwamaitha3924@gmail.com>
2024-09-12 12:28:14 +0300

    modified

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,8 @@
 // src/redux/store.js
+// src/redux/store.js
 import { createStore } from 'redux';
 import teacherReducer from './reducers';
 
 const store = createStore(teacherReducer);
-
-const store = configureStore({
-    reducer: {
-      auth: authReducer,
-    },
-  });
 
 export default store;
